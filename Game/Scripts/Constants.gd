470746017b2bd5c1fee6527d2e307c04b5dc51d0
bswanan <109936808+bswanan@users.noreply.github.com>
2022-10-03 13:34:40 +0800

    Delete Game directory

--- Game/Scripts/Constants.gd
+++
@@ -1,5 +0,0 @@
-#Constants
-class_name Constants
-
-const tile_scn = preload("res://Game/Prefabs/MTile.tscn")
-const num_of_faces = 5
--- Game/Scripts/Panel.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-func _ready():
-	add_to_group("game")
-
-
-func on_gameover(over):
-	visible = over
